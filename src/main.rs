c70a77a34fe2051d83363ae95a21e3001e9fd57c
Riley Lundquist <rlundquist3@gmail.com>
2026-06-25 11:47:09 -0600

    initialize project; add basic sine oscillator

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod oscillators;
+
+use crate::oscillators::{Oscillator, Waveform::Sine};
+use rodio::{DeviceSinkBuilder, Source};
+use std::time::Duration;
+
+// Hardcoded for now; TODO: make this configurable
+pub static SAMPLE_RATE: u32 = 44100;
+
+fn main() {
+    let audio_device =
+        DeviceSinkBuilder::open_default_sink().expect("Should open default audio device");
+
+    let mut sine = Oscillator::new(Sine);
+    sine.set_freq(440.0);
+
+    audio_device
+        .mixer()
+        .add(sine.take_duration(Duration::from_millis(3000)));
+    std::thread::sleep(Duration::from_millis(3000));
+}
---
+++ src/oscillators.rs
@@ -0,0 +1,69 @@
+use rodio::Source;
+use std::{f32::consts::PI, num::NonZero, time::Duration};
+
+use crate::SAMPLE_RATE;
+
+pub enum Waveform {
+    Sine,
+}
+pub use self::Waveform::*;
+
+pub struct Oscillator {
+    waveform: Waveform,
+    phase: f32,
+    phase_delta: f32,
+    last_sample: f32,
+}
+
+impl Oscillator {
+    pub fn new(waveform: Waveform) -> Self {
+        Oscillator {
+            waveform: waveform,
+            phase: 0.0,
+            phase_delta: 0.0,
+            last_sample: 0.0,
+        }
+    }
+
+    pub fn set_freq(&mut self, freq: f32) {
+        self.phase_delta = freq * 2.0 * PI / (SAMPLE_RATE as f32);
+    }
+
+    pub fn next_sample(&mut self) -> f32 {
+        self.phase += self.phase_delta;
+        if self.phase >= 2.0 * PI {
+            self.phase -= 2.0 * PI;
+        }
+
+        self.last_sample = match self.waveform {
+            Sine => self.phase.sin(),
+        };
+        self.last_sample
+    }
+}
+
+impl Source for Oscillator {
+    fn channels(&self) -> NonZero<u16> {
+        NonZero::new(1).unwrap()
+    }
+
+    fn sample_rate(&self) -> NonZero<u32> {
+        NonZero::new(SAMPLE_RATE).unwrap()
+    }
+
+    fn current_span_len(&self) -> Option<usize> {
+        None
+    }
+
+    fn total_duration(&self) -> Option<Duration> {
+        None
+    }
+}
+
+impl Iterator for Oscillator {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<f32> {
+        Some(self.next_sample())
+    }
+}
